d29897e39960e43ce1d925e815bee0d193af0bf7
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2022-05-14 22:44:05 -0300

    Ribbon Balance

--- src/main/java/payroll/payrolms/feignclients/WorkerFeignClients.java
+++ src/main/java/payroll/payrolms/feignclients/WorkerFeignClients.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import payroll.payrolms.entities.Worker;
 
 @Component
-@FeignClient(name = "hr-worker",url="localhost:8001",path = "/workers")
+//@FeignClient(name = "hr-worker",url="localhost:8001",path = "/workers")
+@FeignClient(name = "hr-worker",path = "/workers")
 public interface WorkerFeignClients {
 	
 	@GetMapping(value = "/{id}")
