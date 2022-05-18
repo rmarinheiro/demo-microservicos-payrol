990ab3e772d05cfcd2214afbde18133cd88a27f0
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2022-05-18 20:34:09 -0300

    Alterando configurações para registrar serviço como cliente no eureka server.

--- src/main/java/payroll/payrolms/PayrolMsApplication.java
+++ src/main/java/payroll/payrolms/PayrolMsApplication.java
@@ -2,12 +2,13 @@ package payroll.payrolms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@RibbonClient(name = "hr-worker")
+//@RibbonClient(name = "hr-worker")
+@EnableEurekaClient
 public class PayrolMsApplication {
 
 	public static void main(String[] args) {
